3a52b3ee4e50b8d3625789ecddaedb20e55920de
Alexandros-Eu <alexeouenta@gmail.com>
2025-01-29 01:54:09 +0200

    Lifted input state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
 import Header from './components/Header';
 import Input from './components/Input';
 import Results from './components/Results';
+import { useState } from 'react';
 
 function App() {
+  const [inputData, setInputData] = useState([0, 0, 0, 0]);
+
+  function handleChange(e)
+  {
+    setInputData((oldData => {
+      if(e.target.name === "INITIAL INVESTMENT")
+      {
+        oldData[0] = e.target.value;
+        return [...oldData];
+      }
+
+      if(e.target.name === "ANNUAL INVESTMENT")
+      {
+        oldData[1] = e.target.value;
+        return [...oldData];
+      }
+
+      if(e.target.name === "EXPECTED RETURN")
+      {
+        oldData[2] = e.target.value;
+        return [...oldData];
+      }
+
+      if(e.target.name === "DURATION")
+      {
+        oldData[3] = e.target.value;
+        return [...oldData];
+      }
+
+
+    }))
+  }
+
+
   return (
     <div>
         <Header/>
         <div className="input-group">
-          <Input label="INITIAL INVESTMENT"/>
-          <Input label="ANNUAL INVESTMENT"/>
+          <Input label="INITIAL INVESTMENT" onInput={handleChange} value={inputData[0]}/>
+          <Input label="ANNUAL INVESTMENT" onInput={handleChange} value={inputData[1]}/>
         </div>
         <div className="input-group">
-          <Input label="EXPECTED RETURN"/>
-          <Input label="DURATION"/>
+          <Input label="EXPECTED RETURN" onInput={handleChange} value={inputData[2]}/>
+          <Input label="DURATION" onInput={handleChange} value={inputData[3]}/>
         </div>
         <Results/>
     </div>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,18 +1,9 @@
-import { useState } from 'react';
-
-export default function Input({label})
+export default function Input({label, onInput, value})
 {
-    const [inputData, setInputData] = useState(0);
-
-    function handleChange(e)
-    {
-        setInputData(oldData => e.target.value);
-    }
-
     return (
         <div id="user-input">
             <label htmlFor={label}>{label}</label>
-            <input type="number" name={label} id={label} onChange={handleChange} value={inputData} required/>
+            <input type="number" name={label} id={label} onChange={onInput} value={value} required/>
         </div>
     )
 }
