da898aaf44a5c55b707d48764212b245e8ecf647
Alexandros-Eu <alexeouenta@gmail.com>
2025-01-29 02:19:30 +0200

    Changed inputData state from array to object

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,57 @@
 import Header from './components/Header';
 import Input from './components/Input';
 import Results from './components/Results';
+import { calculateInvestmentResults } from './util/investment.js';
 import { useState } from 'react';
 
 function App() {
-  const [inputData, setInputData] = useState([0, 0, 0, 0]);
+  const [inputData, setInputData] = useState({
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expectedReturn: 0,
+    duration: 0
+  });
 
   function handleChange(e)
   {
     setInputData((oldData => {
       if(e.target.name === "INITIAL INVESTMENT")
       {
-        oldData[0] = e.target.value;
-        return [...oldData];
+        return {...oldData, initialInvestment: e.target.value}
       }
 
       if(e.target.name === "ANNUAL INVESTMENT")
       {
-        oldData[1] = e.target.value;
-        return [...oldData];
+        return {...oldData, annualInvestment: e.target.value}
       }
 
       if(e.target.name === "EXPECTED RETURN")
       {
-        oldData[2] = e.target.value;
-        return [...oldData];
+        return {...oldData, expectedReturn: e.target.value}
       }
 
       if(e.target.name === "DURATION")
       {
-        oldData[3] = e.target.value;
-        return [...oldData];
+        return {...oldData, duration: e.target.value}
       }
-
-
     }))
+
+
   }
+
+  console.log(calculateInvestmentResults(inputData));
 
 
   return (
     <div>
         <Header/>
         <div className="input-group">
-          <Input label="INITIAL INVESTMENT" onInput={handleChange} value={inputData[0]}/>
-          <Input label="ANNUAL INVESTMENT" onInput={handleChange} value={inputData[1]}/>
+          <Input label="INITIAL INVESTMENT" onInput={handleChange} value={inputData.initialInvestment}/>
+          <Input label="ANNUAL INVESTMENT" onInput={handleChange} value={inputData.annualInvestment}/>
         </div>
         <div className="input-group">
-          <Input label="EXPECTED RETURN" onInput={handleChange} value={inputData[2]}/>
-          <Input label="DURATION" onInput={handleChange} value={inputData[3]}/>
+          <Input label="EXPECTED RETURN" onInput={handleChange} value={inputData.expectedReturn}/>
+          <Input label="DURATION" onInput={handleChange} value={inputData.duration}/>
         </div>
         <Results/>
     </div>
